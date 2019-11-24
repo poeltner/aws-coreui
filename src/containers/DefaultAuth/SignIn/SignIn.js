9c9c9ee56d9f92c8e71b9e4d31b9e045e39c0597
Paul <dev@poeltner.co.at>
2019-11-24 21:18:06 +0100

    Update i18n

--- src/containers/DefaultAuth/SignIn/SignIn.js
+++ src/containers/DefaultAuth/SignIn/SignIn.js
@@ -1,36 +1,44 @@
-import React from 'react';
-import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
-import { SignIn } from 'aws-amplify-react';
-import NotificationAlert from 'react-notification-alert';
-import { SignInSocialButtons } from '../SocialButtons/SignInSocialButtons';
-import { withNamespaces } from 'react-i18next';
-import Log from '../../../utils/Logger/Log';
+import React from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardGroup,
+  Col,
+  Container,
+  Form,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Row
+} from "reactstrap";
+import { SignIn } from "aws-amplify-react";
+import NotificationAlert from "react-notification-alert";
+import { SignInSocialButtons } from "../SocialButtons/SignInSocialButtons";
+import { withNamespaces } from "react-i18next";
+import Log from "../../../utils/Logger/Log";
 
 class DefaultSignIn extends SignIn {
   constructor(props) {
     super(props);
 
     this.state = {
-      isLoggingIn: false,
-    }
+      isLoggingIn: false
+    };
     this.onSignIn = this.onSignIn.bind(this);
     this.changeState = this.changeState.bind(this);
   }
 
   error(err) {
     const { t } = this.props;
-    Log.error(err, 'DefaultAuth.SignIn');
+    Log.error(err, "DefaultAuth.SignIn");
     const options = {
-      place: 'tl',
+      place: "tl",
       message: (
-          <div>
-              <div>
-                  { (err.message)?
-                      t(err.message):
-                      t(err) 
-                  }
-              </div>
-          </div> 
+        <div>
+          <div>{err.message ? t(err.message) : t(err)}</div>
+        </div>
       ),
       type: "danger",
       icon: "now-ui-icons ui-1_bell-53",
@@ -45,22 +53,30 @@ class DefaultSignIn extends SignIn {
     } else if (!this.inputs.password) {
       this.error("Password cannot be empty");
     } else {
-      this.setState({isLoggingIn: true});
+      this.setState({ isLoggingIn: true });
       await this.signIn();
-      this.setState({isLoggingIn: false});
+      this.setState({ isLoggingIn: false });
     }
   }
-  
+
   render() {
     const { t } = this.props;
     const { authState, federated, onStateChange } = this.props;
-    if ((authState !== 'signIn') && (authState !== 'signedUp') && (authState !== 'signedOut')) {
+    if (
+      authState !== "signIn" &&
+      authState !== "signedUp" &&
+      authState !== "signedOut"
+    ) {
       return null;
     }
-  
+
     return (
       <div className="app flex-row align-items-center">
-        <NotificationAlert ref={(c) => { this.notify = c; }} />
+        <NotificationAlert
+          ref={c => {
+            this.notify = c;
+          }}
+        />
         <Container>
           <Row className="justify-content-center">
             <Col md="8">
@@ -68,20 +84,23 @@ class DefaultSignIn extends SignIn {
                 <Card className="p-4">
                   <CardBody>
                     <Form>
-                      <h1>{ t('common:Login') }</h1>
-                      <p className="text-muted">{ t('Sign In to your account') }</p>
+                      <h1>{t("common:Login")}</h1>
+                      <p className="text-muted">
+                        {t("Sign In to your account")}
+                      </p>
                       <InputGroup className="mb-3">
                         <InputGroupAddon addonType="prepend">
                           <InputGroupText>
                             <i className="icon-user"></i>
                           </InputGroupText>
                         </InputGroupAddon>
-                        <Input  
-                            autoFocus
-                            placeholder={ t('common:Email') }
-                            key="username"
-                            name="username"
-                            onChange={this.handleInputChange} />
+                        <Input
+                          autoFocus
+                          placeholder={t("common:Email")}
+                          key="username"
+                          name="username"
+                          onChange={this.handleInputChange}
+                        />
                       </InputGroup>
                       <InputGroup className="mb-4">
                         <InputGroupAddon addonType="prepend">
@@ -89,37 +108,58 @@ class DefaultSignIn extends SignIn {
                             <i className="icon-lock"></i>
                           </InputGroupText>
                         </InputGroupAddon>
-                        <Input 
-                            placeholder={ t('common:Password') }
-                            key="password"
-                            type="password"
-                            name="password"
-                            onChange={this.handleInputChange} />
+                        <Input
+                          placeholder={t("common:Password")}
+                          key="password"
+                          type="password"
+                          name="password"
+                          onChange={this.handleInputChange}
+                        />
                       </InputGroup>
                       <Row>
                         <Col xs="6">
-                          <Button color="link" className="px-0" onClick={() => this.changeState('forgotPassword')}>{ t('Forgot password?') }</Button>
+                          <Button
+                            color="link"
+                            className="px-0"
+                            onClick={() => this.changeState("forgotPassword")}
+                          >
+                            {t("Forgot password?")}
+                          </Button>
                         </Col>
                         <Col xs="6" className="text-right">
-                          <Button color="primary" className="px-4" onClick={this.onSignIn}>{ t('common:Login') }{' '} 
-                        { (this.state.isLoggingIn) ? <i className="fa fa-spin fa-circle-o-notch"/>: null }</Button>
-                          
+                          <Button
+                            color="primary"
+                            className="px-4"
+                            onClick={this.onSignIn}
+                          >
+                            {t("common:Login")}{" "}
+                            {this.state.isLoggingIn ? (
+                              <i className="fa fa-spin fa-circle-o-notch" />
+                            ) : null}
+                          </Button>
                         </Col>
                       </Row>
                     </Form>
                     <SignInSocialButtons
-                        onStateChange={onStateChange}
-                        federated={federated}
-                        authState={authState}
-                      />
+                      onStateChange={onStateChange}
+                      federated={federated}
+                      authState={authState}
+                    />
                   </CardBody>
                 </Card>
-                <Card className="text-white bg-primary py-5" >
+                <Card className="text-white bg-primary py-5">
                   <CardBody className="text-center">
                     <div>
-                      <h2>{ t('Sign Up') }</h2>
-                      <p>{ t('Sign Up Text') }</p>
-                      <Button color="primary" className="mt-3" active onClick={() => this.changeState('signUp')}>{ t('Register!') }</Button>
+                      <h2>{t("Sign Up")}</h2>
+                      <p>{t("Sign Up Text")}</p>
+                      <Button
+                        color="primary"
+                        className="mt-3"
+                        active
+                        onClick={() => this.changeState("signUp")}
+                      >
+                        {t("Register!")}
+                      </Button>
                     </div>
                   </CardBody>
                 </Card>
@@ -133,4 +173,4 @@ class DefaultSignIn extends SignIn {
 }
 
 // export default DefaultSignIn;
-export default withNamespaces('auth', { wait: true }) (DefaultSignIn);
+export default withNamespaces("auth", { wait: true })(DefaultSignIn);
